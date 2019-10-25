7e2ebd13c4a21266c12e0aed1e1f1043fdffd297
Zak Barbuto <zbarbuto@nextfaze.com>
2019-10-26 08:37:28 +1030

    Switch to es2015 moodules and add extra publish files

--- bili.config.ts
+++ bili.config.ts
@@ -3,18 +3,17 @@ import { Config } from 'bili'
 const config: Config = {
   plugins: {
     typescript2: {
-      // Override the config in `tsconfig.json`
       tsconfigOverride: {
         include: ['src']
       }
     }
   },
-
-  // Let's take this opportunity to move the CLI flags here as well
   input: 'src/index.ts',
   output: {
     dir: '.',
-    format: ['cjs', 'esm']
+    format: ['cjs', 'umd', 'esm'],
+    sourceMap: true,
+    sourceMapExcludeSources: true
   }
 }
 
